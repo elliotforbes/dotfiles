1a1eeb85fb9d5b48761e2002da8719605d68ee4f
elliotforbes <me@elliotf.dev>
2024-03-14 10:19:23 +0000

    adding more tools to the install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ installTools() {
      brew install bat
      brew install exa
      brew install tokei
+     brew install httpie
+     brew install golangci-lint
+     brew install gh
+     brew install fzf
+     brew install postgresql
+     brew install hashicorp/tap/terraform
 }
 
 setupVim() {
